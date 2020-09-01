9bba11581977995028c6dca929a754f101d78472
mertzamir <mzamir@purdue.edu>
2020-09-01 08:24:32 -0400

    venv and some other updates

--- dict/bin/activate.fish
+++ dict/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mertzamir/DictionaryProject/dict"
+set -gx VIRTUAL_ENV "/home/mertzamir/Web/Dictionary/dict"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(dict) "            
+        if test -n "(dict) "
             printf "%s%s" "(dict) " (set_color normal)
         else
             # ...Otherwise, prepend env
